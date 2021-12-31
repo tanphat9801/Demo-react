db71aa746a411b38751cdf2f22bffd45aa106ea1
LAPTOP-I2B0SOER\TAN PHAT <tanphat9801@gmail.com>
2021-12-31 21:01:01 +0700

    no message

--- lesson1-helloreact/src/App.js
+++ lesson1-helloreact/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <p>hello react</p>
         </a>
       </header>
     </div>
