9a4386a1b7a11c051fc57e0bf78272b6accf358c
LAPTOP-I2B0SOER\TAN PHAT <tanphat9801@gmail.com>
2022-01-02 23:07:33 +0700

    no message

---
+++ lesson1-helloreact/src/views/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Xin chao React 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <p>chao mung den voi react co ban</p>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
